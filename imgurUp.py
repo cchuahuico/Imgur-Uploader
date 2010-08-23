c48521bcb78129e571bca0430e83693f4607e06e
Clarence Chuahuico <cchuahuico@gmail.com>
2010-08-22 18:57:09 -0700

    Added -d/--del options to delete latest uploaded image

--- imgurUp.py
+++ imgurUp.py
@@ -13,16 +13,34 @@ from xml.dom import minidom
 import pycurl
 import StringIO
 import sys
+import tempfile
+import os
+import webbrowser
 
 try:
     import gtk 
 except:
-    print "Only GTK compatible GUI available for now."
+    print "Only GTK compatible systems available for now."
     sys.exit(5);
 
 
+TEMP_DIR = tempfile.getTEMP_DIR()
+TEMP_FILE = "imgurupx.tmp"
+
+def delete_image():
+   try:
+        url = open(os.path.join(TEMP_DIR, TEMP_FILE), "r").read()
+        webbrowser.open(url)
+        # terminate right after opening the browser since 
+        # there is nothing more to be done
+        sys.exit(0)
+    except IOError:
+        print "Temp file doesn't exist or was deleted. Cannot delete image from server."
+
 def parse_opts(): 
-    usage = "usage: %prog [OPTIONS] IMAGE" 
+    usage = """usage: %prog [OPTIONS] IMAGE
+    OR %prog [-d] to delete latest uploaded image"""
+
     parser = OptionParser(usage=usage)
     parser.add_option("-o", "--original", action="store_true", dest="orig",
             help="Get original image size")
@@ -30,16 +48,20 @@ def parse_opts():
             help="Get small thumbnail")
     parser.add_option("-l", "--large", action="store_true", dest="large",
             help="Get large thumbnail")
+    parser.add_option("-d", "--del", action="store_true", dest="delete",
+            help="Delete last uploaded image")
 
     (options, args) = parser.parse_args()
 
+    # only one valid positional argument allowed -- the image file path or name
     if len(args) != 1:
         parser.error("Please specify one valid filename or path")
-        sys.exit(4)
     else:
         filename = args[0]
 
-    # The string returns are names of xml tags that are part of imgur's response
+    if options.delete:
+        delete_image()
+    # The strings returned are names of xml tags that are part of imgur's response
     # to the request
     if options.small:
         return ("small_thumbnail", filename)
@@ -48,7 +70,12 @@ def parse_opts():
     else:
         return ("original_image", filename)
         
-
+def write_old_link(url):
+    try:
+        open(os.path.join(TEMP_DIR, TEMP_FILE), "w").write(url)
+    except IOError:
+        print "Unable to create temp file."
+    
 def main():
     (size, image_name) = parse_opts()
 
@@ -72,6 +99,8 @@ def main():
     # grab the contents of the value under specific size tag
     # ie. <original_image>http://www.imgur.com/xYz</original_image>
     img_link = doc.getElementsByTagName(size)[0].firstChild.nodeValue
+
+    write_old_link(doc.getElementsByTagName("delete_page")[0].firstChild.nodeValue)
     
     clipboard = gtk.clipboard_get()
     clipboard.set_text(img_link)
